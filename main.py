abe48c666b8342d7092655e9b7c52766020209a1
ziwen <zzws524@sina.com>
2019-03-10 17:55:40 +0800

    finish parse row

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from ziwenLog import myLogConfig
 
@@ -20,7 +21,27 @@ firstCrisisCountry='AN'
 def parseCsvFile(fileName):
     with open(fileName,'r') as f:
         myLines=f.readlines()
-    logger.info(myLines[0])
+    logger.info('Totally %s lines (including title) will be analyzed.'%str(len(myLines)))
+    totalRowCnt=len(myLines)
+
+    if ((myLines[0].strip().split(','))[0] != "DS"):
+        logger.error('First chars of first line should be DS')
+        os._exit(1)
+
+    logger.info('Totall %s countries will be analyzed.'%str(len(myLines[0].strip().split(','))-1))
+    totalColCnt=len(myLines[0].strip().split(','))
+
+    dataRowDict={};
+    dataColDict={};
+    for i in range(1,len(myLines)): #i is the row number -1
+        tmpDict={}
+        tmpDict['country']=myLines[i].strip().split(',')[0]
+        tmpDataList=[]
+        for j in range(1,len(myLines[i].strip().split(','))):
+            tmpDataList.append(myLines[i].strip().split(',')[j])
+        tmpDict['data']=tmpDataList
+        dataRowDict[i+1]=tmpDict  #dataRowDict[0]->{'country':'AD','data':[0,0,...]}
+
 
 if __name__=='__main__':
     myLog=myLogConfig.ConfigMyLog(logFileName='DataProcess',withFolder=False)
